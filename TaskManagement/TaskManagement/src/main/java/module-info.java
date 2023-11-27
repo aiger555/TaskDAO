416717347eed1f0f92a6c750e9ceb4bc6456e80a
Aigerim Nuralieva <75246512+aiger555@users.noreply.github.com>
2023-11-27 14:15:19 +0600

    Delete TaskManagement directory

--- TaskManagement/TaskManagement/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.taskmanagement {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.taskmanagement to javafx.fxml;
-    exports com.example.taskmanagement;
-}
